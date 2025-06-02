b03192d429fccae5a6ae2418671538dbb42e5b21
Drapliks <kinstevg@gmail.com>
2025-06-03 01:18:16 +0500

    Update Installer.sh

--- Installer.sh
+++ Installer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd dotfiles-hyprland
+cd DCHC
 sudo pacman -R dophin
 sudo pacman -S nwg-look thunar fastfetch waybar papirus-icon-theme hyprpaper mako wl-clipboard wl-clip-persist cliphist xdg-desktop-portal-hyprland grim slurp ttf-font-awesome otf-font-awesome cava nyancat caca-utils cmatrix dotnet-sdk lib32-nvidia-utils steam obs-studio spotify-luancher adobe-source-han-sans-jp-fonts adobe-source-han-sans-cn-fonts mission-center thunar udiskie discord gimp pycharm-community-edition gnome-boxes fish ark flatpak nvidia nvidia-utils
 git clone https://aur.archlinux.org/paru.git
