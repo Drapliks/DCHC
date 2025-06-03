38775ba9a427a4f2b391424af9413aa99d1c6fcc
Drapliks <kinstevg@gmail.com>
2025-06-03 22:10:56 +0500

    Update Installer-amd.sh

--- Installer-amd.sh
+++ Installer-amd.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo pacman -R dolphin
-sudo pacman -S nwg-look thunar fastfetch waybar papirus-icon-theme hyprpaper mako wl-clipboard wl-clip-persist cliphist xdg-desktop-portal-hyprland grim slurp ttf-font-awesome otf-font-awesome cava nyancat libcaca cmatrix dotnet-sdk steam obs-studio spotify-launcher adobe-source-han-sans-jp-fonts adobe-source-han-sans-cn-fonts mission-center udiskie discord gimp pycharm-community-edition gnome-boxes fish ark flatpak
+sudo pacman -S nwg-look thunar fastfetch waybar papirus-icon-theme hyprpaper mako wl-clipboard wl-clip-persist cliphist xdg-desktop-portal-hyprland grim slurp ttf-font-awesome otf-font-awesome cava nyancat libcaca cmatrix dotnet-sdk obs-studio spotify-launcher adobe-source-han-sans-jp-fonts adobe-source-han-sans-cn-fonts mission-center udiskie discord gimp pycharm-community-edition gnome-boxes fish ark flatpak
 cd
 git clone https://aur.archlinux.org/paru.git
 cd paru
