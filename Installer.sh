e82051f81dbdd6744fc8c5697008e625bb4eb1c6
Drapliks <kinstevg@gmail.com>
2025-06-03 20:50:47 +0500

    Update Installer.sh

--- Installer.sh
+++ Installer.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 sudo pacman -R dolphin
 sudo pacman -S nwg-look thunar fastfetch waybar papirus-icon-theme hyprpaper mako wl-clipboard wl-clip-persist cliphist xdg-desktop-portal-hyprland grim slurp ttf-font-awesome otf-font-awesome cava nyancat libcaca cmatrix dotnet-sdk lib32-nvidia-utils steam obs-studio spotify-luancher adobe-source-han-sans-jp-fonts adobe-source-han-sans-cn-fonts mission-center udiskie discord gimp pycharm-community-edition gnome-boxes fish ark flatpak nvidia nvidia-utils
-git clone https://aur.archlinux.org/paru.git
 cd
+git clone https://aur.archlinux.org/paru.git
 cd paru
 makepkg -si
 git clone https://github.com/pipeseroni/pipes.sh.git
