0bf8d76d9174fd90e36e8e49a8f846fd72dbc763
Drapliks <kinstevg@gmail.com>
2025-06-03 21:00:19 +0500

    Add files via upload

---
+++ Installer.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+sudo pacman -R dolphin
+sudo pacman -S nwg-look thunar fastfetch waybar papirus-icon-theme hyprpaper mako wl-clipboard wl-clip-persist cliphist xdg-desktop-portal-hyprland grim slurp ttf-font-awesome otf-font-awesome cava nyancat libcaca cmatrix dotnet-sdk lib32-nvidia-utils steam obs-studio spotify-launcher adobe-source-han-sans-jp-fonts adobe-source-han-sans-cn-fonts mission-center udiskie discord gimp pycharm-community-edition gnome-boxes fish ark flatpak nvidia nvidia-utils
+cd
+git clone https://aur.archlinux.org/paru.git
+cd paru
+makepkg -si
+cd
+git clone https://github.com/pipeseroni/pipes.sh.git
+cd pipes.sh
+sudo make install
+cd
+paru -S sublime-text-4 unityhub visual-studio-code-bin heroic-games-launcher
+cd DCHC
+mv config ~/
+mv wallpaper ~/
+chsh -s /usr/bin/fish
