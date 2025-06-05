469f3ffed9c947eb1b269eb3b33bbeb6b30e3b9e
Drapliks <kinstevg@gmail.com>
2025-06-05 15:21:42 +0500

    Update Installer-nvidia.sh

--- Installer-nvidia.sh
+++ Installer-nvidia.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo pacman -R dolphin
-sudo pacman -S nwg-look thunar fastfetch waybar papirus-icon-theme hyprpaper mako wl-clipboard wl-clip-persist cliphist xdg-desktop-portal-hyprland grim slurp ttf-font-awesome otf-font-awesome cava nyancat libcaca cmatrix dotnet-sdk obs-studio spotify-launcher adobe-source-han-sans-jp-fonts adobe-source-han-sans-cn-fonts mission-center udiskie discord gimp pycharm-community-edition gnome-boxes fish ark flatpak nvidia nvidia-utils
+sudo pacman -S nwg-look thunar fastfetch waybar papirus-icon-theme hyprpaper mako wl-clipboard wl-clip-persist cliphist xdg-desktop-portal-hyprland grim slurp ttf-font-awesome otf-font-awesome cava nyancat libcaca cmatrix dotnet-sdk obs-studio spotify-launcher adobe-source-han-sans-jp-fonts adobe-source-han-sans-cn-fonts mission-center udiskie discord gimp pycharm-community-edition gnome-boxes fish ark flatpak nvidia nvidia-utils vlc
 cd
 git clone https://aur.archlinux.org/paru.git
 cd paru
@@ -10,7 +10,7 @@ git clone https://github.com/pipeseroni/pipes.sh.git
 cd pipes.sh
 sudo make install
 cd
-paru -S sublime-text-4 unityhub visual-studio-code-bin heroic-games-launcher
+paru -S sublime-text-4 unityhub visual-studio-code-bin heroic-games-launcher qview
 cd DCHC
 mv config ~/
 mv wallpaper ~/
