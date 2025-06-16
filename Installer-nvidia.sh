0608c427503318bad75cdf8247d814c902753669
Drapliks <kinstevg@gmail.com>
2025-06-16 21:35:39 +0000

    Update Installer-nvidia.sh

--- Installer-nvidia.sh
+++ Installer-nvidia.sh
@@ -2,15 +2,15 @@
 sudo pacman -R dolphin
 sudo pacman -S nwg-look thunar fastfetch waybar papirus-icon-theme hyprpaper mako wl-clipboard wl-clip-persist cliphist xdg-desktop-portal-hyprland grim slurp ttf-font-awesome otf-font-awesome cava nyancat libcaca cmatrix dotnet-sdk obs-studio spotify-launcher adobe-source-han-sans-jp-fonts adobe-source-han-sans-cn-fonts mission-center udiskie discord gimp pycharm-community-edition gnome-boxes fish ark flatpak nvidia nvidia-utils vlc noto-fonts-emoji pavucontrol
 cd
-git clone https://aur.archlinux.org/paru.git
-cd paru
+git https://aur.archlinux.org/yay.git
+cd yay
 makepkg -si
 cd
 git clone https://github.com/pipeseroni/pipes.sh.git
 cd pipes.sh
 sudo make install
 cd
-paru -S sublime-text-4 unityhub visual-studio-code-bin qview wine
+yay -S sublime-text-4 unityhub visual-studio-code-bin qview wine google-chrome
 cd DCHC
 mv config ~/
 mv wallpaper ~/
