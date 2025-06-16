4eb2eff9edbafdc3067fa3d87a4d2a5387699b0c
Drapliks <kinstevg@gmail.com>
2025-06-16 21:54:52 +0000

    Update Installer-nvidia.sh

--- Installer-nvidia.sh
+++ Installer-nvidia.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo pacman -R dolphin
-sudo pacman -S nwg-look thunar fastfetch waybar papirus-icon-theme hyprpaper mako wl-clipboard wl-clip-persist cliphist xdg-desktop-portal-hyprland grim slurp ttf-font-awesome otf-font-awesome cava nyancat libcaca cmatrix dotnet-sdk obs-studio spotify-launcher adobe-source-han-sans-jp-fonts adobe-source-han-sans-cn-fonts mission-center udiskie discord gimp pycharm-community-edition gnome-boxes fish ark flatpak nvidia nvidia-utils vlc noto-fonts-emoji pavucontrol
+sudo pacman -S nwg-look thunar fastfetch waybar papirus-icon-theme hyprpaper mako wl-clipboard wl-clip-persist cliphist xdg-desktop-portal-hyprland grim slurp ttf-font-awesome otf-font-awesome cava nyancat libcaca cmatrix dotnet-sdk obs-studio spotify-launcher adobe-source-han-sans-jp-fonts adobe-source-han-sans-cn-fonts mission-center udiskie discord gimp pycharm-community-edition gnome-boxes fish ark flatpak nvidia nvidia-utils vlc noto-fonts-emoji pavucontrol firefox
 cd
 git https://aur.archlinux.org/yay.git
 cd yay
@@ -17,3 +17,4 @@ mv wallpaper ~/
 cd
 rm -rf DCHC
 chsh -s /usr/bin/fish
+xdg-settings set default-web-browser firefox.desktop
