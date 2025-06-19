af9a54b4641d59c9513634cb2015a5eeb3fdea6b
Drapliks <kinstevg@gmail.com>
2025-06-19 20:27:42 +0500

    Update Installer-nvidia.sh

--- Installer-nvidia.sh
+++ Installer-nvidia.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo pacman -R dolphin
-sudo pacman -S nwg-look thunar fastfetch waybar papirus-icon-theme hyprpaper mako wl-clipboard wl-clip-persist cliphist xdg-desktop-portal-hyprland grim slurp ttf-font-awesome otf-font-awesome cava nyancat libcaca cmatrix dotnet-sdk obs-studio spotify-launcher adobe-source-han-sans-jp-fonts adobe-source-han-sans-cn-fonts mission-center udiskie gimp pycharm-community-edition gnome-boxes fish ark flatpak nvidia nvidia-utils vlc noto-fonts-emoji pavucontrol firefox
+sudo pacman -S nwg-look thunar fastfetch waybar papirus-icon-theme hyprpaper mako wl-clipboard wl-clip-persist cliphist xdg-desktop-portal-hyprland grim slurp ttf-font-awesome otf-font-awesome cava dotnet-sdk cmus obs-studio adobe-source-han-sans-jp-fonts adobe-source-han-sans-cn-fonts mission-center udiskie gimp pycharm-community-edition gnome-boxes fish ark flatpak nvidia nvidia-utils vlc noto-fonts-emoji pavucontrol firefox
 cd
 git https://aur.archlinux.org/yay.git
 cd yay
