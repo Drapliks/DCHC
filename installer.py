55f17ccb019669229e3b9b42d6ef8d4bb2653238
Drapliks <kinstevg@gmail.com>
2026-01-08 19:47:52 +0000

    Update installer.py

--- installer.py
+++ installer.py
@@ -5,27 +5,31 @@ os.system('cd ~/DCHC')
 os.system('clear')
 print("Welcome to DCHC installer!\n\nChoose your action:\n1.Start installer\n2.Exit")
 act = input()
-while True:
-    if act == "1":
-        zapret = input("Do you want install zapret-discord-youtube?(y/n)")
-        dev = input("Do you want install development programs?(y/n)")
-        pipes = input("Do you want install pipes.sh?(y/n)")
-        os.system('chmod +x ~/DCHC/components/yayI.sh')
-        os.system('bash ~/DCHC/components/yayI.sh')
-        os.system('chmod +x ~/DCHC/components/main.sh')
-        os.system('bash ~/DCHC/components/main.sh')
-        if zapret == "y":
-            os.system('chmod +x ~/DCHC/components/zapret.sh')
-            os.system('bash ~/DCHC/components/zapret.sh')
-        if dev == "y":
-            os.system('chmod +x ~/DCHC/components/dev.sh')
-            os.system('bash ~/DCHC/components/dev.sh')
-        if pipes == "y":
-            os.system('chmod +x ~/DCHC/components/pipesI.sh')
-            os.system('bash ~/DCHC/components/pipesI.sh')
-        os.system('chmod +x ~/DCHC/components/mr.sh')
-        os.system('bash ~/DCHC/components/mr.sh')
-		print("Reboot your PC!")
-		sys.exit()
-    else:
-        sys.exit()
+
+if act == "1":
+    zapret = input("Do you want install zapret-discord-youtube?(y/n) ")
+    dev = input("Do you want install development programs?(y/n) ")
+    pipes = input("Do you want install pipes.sh?(y/n) ")
+    
+    os.system('chmod +x ~/DCHC/components/yayI.sh')
+    os.system('bash ~/DCHC/components/yayI.sh')
+    os.system('chmod +x ~/DCHC/components/main.sh')
+    os.system('bash ~/DCHC/components/main.sh')
+    
+    if zapret == "y":
+        os.system('chmod +x ~/DCHC/components/zapret.sh')
+        os.system('bash ~/DCHC/components/zapret.sh')
+    if dev == "y":
+        os.system('chmod +x ~/DCHC/components/dev.sh')
+        os.system('bash ~/DCHC/components/dev.sh')
+    if pipes == "y":
+        os.system('chmod +x ~/DCHC/components/pipesI.sh')
+        os.system('bash ~/DCHC/components/pipesI.sh')
+    
+    os.system('chmod +x ~/DCHC/components/mr.sh')
+    os.system('bash ~/DCHC/components/mr.sh')
+    
+    print("Reboot your PC!")
+    sys.exit()
+else:
+    sys.exit()
