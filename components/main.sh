fd220199e9bf64cac2cf59ee0716a8c498c33405
Drapliks <kinstevg@gmail.com>
2026-01-09 20:28:22 +0000

    Update main.sh

--- components/main.sh
+++ components/main.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo pacman -S nwg-look fastfetch waybar papirus-icon-theme hyprpaper mako wl-clipboard wl-clip-persist cliphist xdg-desktop-portal-hyprland grim slurp ttf-font-awesome otf-font-awesome cava obs-studio adobe-source-han-sans-jp-fonts adobe-source-han-sans-cn-fonts udiskie fish ark flatpak mpv noto-fonts-emoji pavucontrol stow
+sudo pacman -S nwg-look fastfetch waybar papirus-icon-theme hyprpaper mako wl-clipboard wl-clip-persist cliphist xdg-desktop-portal-hyprland grim slurp ttf-font-awesome otf-font-awesome cava obs-studio adobe-source-han-sans-jp-fonts adobe-source-han-sans-cn-fonts udiskie fish ark flatpak mpv noto-fonts-emoji pavucontrol nvim
 yay -S qview wine yandex-music
